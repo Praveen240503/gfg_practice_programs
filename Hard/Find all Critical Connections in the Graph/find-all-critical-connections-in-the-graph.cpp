1c19b9f34bfd94197cdac8eb26b6938e733a5458
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-02-14 23:37:56 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Find all Critical Connections in the Graph/find-all-critical-connections-in-the-graph.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    int timer=0;
+    
+    void dfs(int node, int parent, vector<int>&vis, vector<int>adj[], 
+        vector<vector<int>> &bridges, int tin[], int low[]){
+            vis[node]=1;
+            tin[node]=low[node]=timer;
+            timer++;
+            for(auto it : adj[node]){
+                if(it == parent) continue;
+                
+                if(vis[it]==0){
+                    dfs(it, node, vis, adj, bridges, tin, low);
+                    
+                    low[node] = min(low[node], low[it]);
+                    
+                    if(low[it] > tin[node]){
+                        if(it>node){
+                            bridges.push_back({node, it});
+                        }
+                        else{
+                            bridges.push_back({it, node});
+                        }
+                    }
+                }
+                else{
+                    low[node] = min(low[node], low[it]);
+                }
+            }
+        }
+    
+	vector<vector<int>>criticalConnections(int v, vector<int> adj[]){
+	    // Code here
+	    vector<int>vis(v,0);
+	    int tin[v];
+	    int low[v];
+	    
+	    vector<vector<int>>bridges;
+	    dfs(0, -1, vis, adj, bridges, tin, low);
+	    sort(bridges.begin(),bridges.end());
+	    return bridges;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main(){
+	int tc;
+    cin >> tc;
+    while(tc--){
+        int V, E;
+        cin >> V >> E;
+        vector<int>adj[V];
+        for(int i = 0; i < E; i++){
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        vector<vector<int>>ans = obj.criticalConnections(V, adj);
+        
+        for(int i=0;i<ans.size();i++)
+            cout<<ans[i][0]<<" "<<ans[i][1]<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
